07f04f223e639704bf2f7a1f60a38f7068622f4a
Fayozxon2909 <aslonovfayozxon2@gmail.com>
2025-07-16 14:59:58 +0500

    Update events.py

--- handlers/events.py
+++ handlers/events.py
@@ -1,6 +1,5 @@
 from aiogram import Router
 from aiogram.types import Message
-from aiogram.types import ChatMemberUpdated
 
 router = Router()
 
@@ -16,16 +15,3 @@ async def handle_new_members(message: Message):
         await message.answer(
             f"❌ <{message.left_chat_member.full_name} guruhdan chiqib ketdi."
         )
-@router.chat_member()
-async def user_left_or_kicked(event: ChatMemberUpdated):
-    old_status = event.old_chat_member.status
-    new_status = event.new_chat_member.status
-
-    if old_status in ("member", "administrator") and new_status == "left":
-        user = event.old_chat_member.user.full_name
-        by_who = event.from_user.full_name
-
-        if event.from_user.id == event.old_chat_member.user.id:
-            await event.answer(f"❌ {user} guruhdan o‘zi chiqib ketdi.")
-        else:
-            await event.answer(f"❌ {by_who} {user} ni guruhdan chiqazdi.")
